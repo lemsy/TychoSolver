3e5085b19a79908bbcc0a861a13d3ad6027edc74
Ismel <diomedes.martnezdaz2@gmail.com>
2025-07-04 10:51:09 +0200

    feat: refactor MutationOperator to support generic mutation logic and configurable mutation rate

--- src/algorithms/genetic/components/MutationOperator.ts
+++ src/algorithms/genetic/components/MutationOperator.ts
@@ -1,12 +1,23 @@
 import { MutationOperator } from '../../../core/operators/MutationOperator';
 
-// Bit-flip mutation for array-based binary individuals
-export class MutationOperatorImpl<T extends number[]> implements MutationOperator<T> {
+// Generic mutation operator for array-based individuals
+export class MutationOperatorImpl<T extends any[]> implements MutationOperator<T> {
+    private geneMutator: (gene: T[number], index: number, individual: T) => T[number];
+    private mutationRate: number;
+
+    constructor(
+        geneMutator: (gene: T[number], index: number, individual: T) => T[number],
+        mutationRate?: number
+    ) {
+        this.geneMutator = geneMutator;
+        this.mutationRate = mutationRate ?? 1;
+    }
+
     mutate(individual: T): T {
-        const mutationRate = 1 / individual.length;
-        const mutated = individual.map(gene => {
-            if (Math.random() < mutationRate) {
-                return gene === 0 ? 1 : 0;
+        const rate = this.mutationRate / individual.length;
+        const mutated = individual.map((gene, idx) => {
+            if (Math.random() < rate) {
+                return this.geneMutator(gene, idx, individual);
             }
             return gene;
         });
